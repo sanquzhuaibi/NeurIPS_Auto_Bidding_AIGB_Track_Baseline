850dc8894d5b3ef0398105ba3f7977f20d7e11be
ziyou <ziyou@ziyoudeMacBook-Pro.local>
2024-08-20 17:29:02 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t registry.cn-beijing.aliyuncs.com/ocpm_rl/test1:test_5 -f ./Dockerfile .
-docker push registry.cn-beijing.aliyuncs.com/ocpm_rl/test1:test_5
+docker build -t registry.cn-beijing.aliyuncs.com/ocpm_rl/test1:test_7 -f ./Dockerfile .
+docker push registry.cn-beijing.aliyuncs.com/ocpm_rl/test1:test_7
 
 #sudo gpasswd -a $USER docker
 #newgrp docker
